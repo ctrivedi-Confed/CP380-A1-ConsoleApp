7a0dac4d7dbbce861f5ee3914d157ba57f86bc0e
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-06-22 01:01:59 -0400

    tests are passed and has an output ss in repo

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -22,6 +22,7 @@ namespace RatingAdjustment.Services
         void SetPercentPositive(double stars)
         {
             // TODO: Implement this!
+            _percent_positive =  stars / MAX_STARS;
         }
 
         /**
@@ -29,7 +30,14 @@ namespace RatingAdjustment.Services
          */
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
+            // TODO: Implement this!
+            _q = Z * Math.Sqrt(
+                (
+                (_percent_positive * (1.0 - _percent_positive))
+                + ((Z * Z) / (4.0 * number_of_ratings))
+                ) / number_of_ratings
+                );
+
         }
 
         /** Adjusted lower bound
@@ -40,7 +48,18 @@ namespace RatingAdjustment.Services
          */
         public double Adjust(double stars, double number_of_ratings) {
             // TODO: Implement this!
-            return 0.0;
+
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+
+            var lowerBound = ((_percent_positive + ((Z * Z) / (2.0 * number_of_ratings)) - _q) 
+                / (1.0 + ((Z * Z) / number_of_ratings)));
+
+            var lb = lowerBound * MAX_STARS;
+            if(lb < MAX_STARS)
+                return lb;
+            else
+                return stars;
         }
     }
 }
